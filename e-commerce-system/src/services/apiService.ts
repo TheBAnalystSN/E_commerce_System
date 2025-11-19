ea9bbceed80ae65d197e49a2a7aae61ffb3469c0
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2025-11-19 17:22:02 -0500

    Asynchronous Operations

---
+++ e-commerce-system/src/services/apiService.ts
@@ -0,0 +1,20 @@
+import { AppError } from "../utils/errorHandler";
+
+
+const API_URL = "https://dummyjson.com/products";
+
+
+export async function fetchProducts() {
+try {
+const res = await fetch(API_URL);
+if (!res.ok) {
+throw new AppError("Failed to fetch products");
+}
+
+
+const data = await res.json();
+return data.products;
+} catch (err) {
+throw new AppError("Unable to fetch API data");
+}
+}
