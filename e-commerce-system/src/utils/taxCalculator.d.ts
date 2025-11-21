18dcd38ccb52df0289e8f245dfed403befb3ab49
[Dustin Robertson] <mrdustinrobertson@gmail.com>
2025-11-21 15:55:32 -0500

    I just broke everthing.

--- e-commerce-system/src/utils/taxCalculator.d.ts
+++ e-commerce-system/src/utils/taxCalculator.d.ts
@@ -1,2 +1,2 @@
-export {};
+export declare function calculateTax(price: number, category: string): number;
 //# sourceMappingURL=taxCalculator.d.ts.map
--- e-commerce-system/src/utils/taxCalculator.js
+++ e-commerce-system/src/utils/taxCalculator.js
@@ -1,3 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.calculateTax = calculateTax;
+function calculateTax(price, category) {
+    let taxRate = 0.0475;
+    if (category === "groceries") {
+        taxRate = 0.03;
+    }
+    let tax = price * taxRate;
+    return tax;
+}
 //# sourceMappingURL=taxCalculator.js.map
